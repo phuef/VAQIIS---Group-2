b9fb1ec2eac13cac0a1b5954f4df23a89ed59c6c
Henry Fock <h_fock01@wwu.de>
2019-10-31 11:57:51 +0100

    get database content
    get the whole databse content as json http response from /get_all/ endpoint

--- server/server/routes.py
+++ server/server/routes.py
@@ -1,17 +1,30 @@
-from flask import redirect, render_template, url_for, request 
+from flask import redirect, render_template, url_for, request
 from server import app
 from .model import Airpolution
+import json
 
-@app.route('/')
+
+@app.route("/")
 def index():
 	pass
 
 
-@app.route('/add', methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add():
 	pass
 
 
-@app.route('/search')
-def search():
-	print(Airpolution.query.all())
+@app.route("/get_all")
+def get_all():
+	a = Airpolution.query.all()
+	print(a)
+	data = {"status": "OK", "data":[]}
+	for point in a:
+		data["data"].append({
+			"id": point.id,
+			"coordinates": [point.latitude, point.longitude],
+			"data": point.data,
+			"city": point.city,
+			"street": point.street
+		})
+	return json.dumps(data)

2808044a831a58838b7f62d1069d06b27f9d3ea7
Henry Fock <h_fock01@wwu.de>
2019-11-04 12:53:45 +0100

    add demo data to database

--- server/server/routes.py
+++ server/server/routes.py
@@ -1,30 +1,31 @@
 from flask import redirect, render_template, url_for, request
-from server import app
+from server import app, db
 from .model import Airpolution
+from .tools import model_to_json
 import json
 
 
 @app.route("/")
 def index():
-	pass
+    pass
 
 
 @app.route("/add", methods=["POST"])
 def add():
-	pass
+    data_point = Airpolution(
+        longitude=7.87345867, latitude=51.834568, data=0.5, city="Muster", street="city"
+    )
+    db.session.add(data_point)
+    db.session.commit()
+    print(data_point)
+    return {"status": "OK", "data": [model_to_json(data_point)]}
 
 
 @app.route("/get_all")
 def get_all():
-	a = Airpolution.query.all()
-	print(a)
-	data = {"status": "OK", "data":[]}
-	for point in a:
-		data["data"].append({
-			"id": point.id,
-			"coordinates": [point.latitude, point.longitude],
-			"data": point.data,
-			"city": point.city,
-			"street": point.street
-		})
-	return json.dumps(data)
+    a = Airpolution.query.all()
+    print(a)
+    data = {"status": "OK", "data": []}
+    for point in a:
+        data["data"].append(model_to_json(point))
+    return json.dumps(data)
